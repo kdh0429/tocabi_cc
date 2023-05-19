30bd25516ee046df129858249247b6065328edfc
Donghyeon Kim <kdh0429@snu.ac.kr>
2023-05-20 08:20:16 +0900

    Fine-tuned on rough terrain

--- include/cc.h
+++ include/cc.h
@@ -34,12 +34,10 @@ public:
     static const int num_actuator_action = 12;
     static const int num_cur_state = 44;
     static const int num_cur_internal_state = 31;
-    static const int num_state_skip = 1;
+    static const int num_state_skip = 2;
     static const int num_state_hist = 5;
     static const int num_state = num_cur_internal_state*num_state_hist+num_action*(num_state_hist-1);
     static const int num_hidden = 256;
-
-    static const int num_ref_motion = 3600;
 
     Eigen::MatrixXd policy_net_w0_;
     Eigen::MatrixXd policy_net_b0_;
@@ -64,8 +62,6 @@ public:
     bool stop_by_value_thres_ = false;
     Eigen::Matrix<double, MODEL_DOF, 1> q_stop_;
     float stop_start_time_;
-    
-    Eigen::MatrixXd ref_motion_;
     
     Eigen::MatrixXd state_;
     Eigen::MatrixXd state_cur_;
