4ab2ad713960a6f1207f22c3e0240acf84e4ab22
Donghyeon Kim <kdh0429@snu.ac.kr>
2023-09-14 19:10:21 +0900

    Static fricion identification

--- include/cc.h
+++ include/cc.h
@@ -23,59 +23,14 @@ public:
     RobotData rd_cc_;
 
     //////////////////////////////////////////// Donghyeon RL /////////////////////////////////////////
-    void loadNetwork();
     void processNoise();
-    void processObservation();
-    void feedforwardPolicy();
     void initVariable();
-    Eigen::Vector3d mat2euler(Eigen::Matrix3d mat);
-
-    static const int num_action = 13;
-    static const int num_actuator_action = 12;
-    static const int num_cur_state = 44;
-    static const int num_cur_internal_state = 31;
-    static const int num_state_skip = 2;
-    static const int num_state_hist = 5;
-    static const int num_state = num_cur_internal_state*num_state_hist+num_action*(num_state_hist-1);
-    static const int num_hidden = 256;
-
-    Eigen::MatrixXd policy_net_w0_;
-    Eigen::MatrixXd policy_net_b0_;
-    Eigen::MatrixXd policy_net_w2_;
-    Eigen::MatrixXd policy_net_b2_;
-    Eigen::MatrixXd action_net_w_;
-    Eigen::MatrixXd action_net_b_;
-    Eigen::MatrixXd hidden_layer1_;
-    Eigen::MatrixXd hidden_layer2_;
-    Eigen::MatrixXd rl_action_;
-
-    Eigen::MatrixXd value_net_w0_;
-    Eigen::MatrixXd value_net_b0_;
-    Eigen::MatrixXd value_net_w2_;
-    Eigen::MatrixXd value_net_b2_;
-    Eigen::MatrixXd value_net_w_;
-    Eigen::MatrixXd value_net_b_;
-    Eigen::MatrixXd value_hidden_layer1_;
-    Eigen::MatrixXd value_hidden_layer2_;
-    double value_;
-
-    bool stop_by_value_thres_ = false;
-    Eigen::Matrix<double, MODEL_DOF, 1> q_stop_;
-    float stop_start_time_;
-    
-    Eigen::MatrixXd state_;
-    Eigen::MatrixXd state_cur_;
-    Eigen::MatrixXd state_buffer_;
-    Eigen::MatrixXd state_mean_;
-    Eigen::MatrixXd state_var_;
+    void moveStretchPose(float traj_start_time, float traj_duration, Eigen::Matrix<double, MODEL_DOF, 1> q_init);
 
     std::ofstream writeFile;
 
-    float phase_ = 0.0;
-
     bool is_on_robot_ = false;
     bool is_write_file_ = true;
-    Eigen::Matrix<double, MODEL_DOF, 1> q_dot_lpf_;
 
     Eigen::Matrix<double, MODEL_DOF, 1> q_init_;
     Eigen::Matrix<double, MODEL_DOF, 1> q_noise_;
@@ -84,33 +39,26 @@ public:
 
     Eigen::Matrix<double, MODEL_DOF, 1> torque_init_;
     Eigen::Matrix<double, MODEL_DOF, 1> torque_spline_;
-    Eigen::Matrix<double, MODEL_DOF, 1> torque_rl_;
-    Eigen::Matrix<double, MODEL_DOF, 1> torque_bound_;
+    Eigen::Matrix<double, MODEL_DOF, 1> torque_stretch_;
 
     Eigen::Matrix<double, MODEL_DOF, MODEL_DOF> kp_;
     Eigen::Matrix<double, MODEL_DOF, MODEL_DOF> kv_;
 
     float start_time_;
-    float time_inference_pre_ = 0.0;
     float time_write_pre_ = 0.0;
 
     double time_cur_;
     double time_pre_;
-    double action_dt_accumulate_ = 0.953;
 
-    Eigen::Vector3d euler_angle_;
+    int inspect_joint_idx_ = 0;
+    float inspect_torque_;
 
-    // float ft_left_init_ = 500.0;
-    // float ft_right_init_ = 500.0;
-
-    // Joystick
-    ros::NodeHandle nh_;
-
-    void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
-    ros::Subscriber joy_sub_;
-
-    double target_vel_x_ = 0.0;
-    double target_vel_y_ = 0.0;
+    float inspect_time_;
+    float inspect_joint_finish_time_;
+    float inspect_torque_change_time_;
+    float torque_change_interval_ = 1.0;
+    float torque_increment_ = 0.05;
+    float q_dot_threshold_ = 0.05;
 
 private:
     Eigen::VectorQd ControlVal_;
