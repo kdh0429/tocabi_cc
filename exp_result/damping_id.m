215694e74c3d0810180b7f299c06edf28ddcda59
Donghyeon Kim <kdh0429@snu.ac.kr>
2023-09-19 21:17:22 +0900

    Update sim result

--- exp_result/damping_id.m
+++ exp_result/damping_id.m
@@ -2,11 +2,12 @@ clear all
 
 figure()
 for i=1:12
-    d_real = load(strcat('trial1/data_joint_',int2str(i-1),'.csv'));
+    d_real = load(strcat('trial2/data_joint_',int2str(i-1),'.csv'));
     d_sim_1 = load(strcat('sim_1.32_0.22/data_joint_',int2str(i-1),'.csv'));
     d_sim_2 = load(strcat('sim_idfriction_0.22/data_joint_',int2str(i-1),'.csv'));
-    d_sim_3 = load(strcat('sim_idfriction_5.22/data_joint_',int2str(i-1),'.csv'));
-    d_sim_4 = load(strcat('sim_idfriction_15.22/data_joint_',int2str(i-1),'.csv'));
+%     d_sim_3 = load(strcat('sim_idfriction_5.22/data_joint_',int2str(i-1),'.csv'));
+%     d_sim_4 = load(strcat('sim_idfriction_15.22/data_joint_',int2str(i-1),'.csv'));
+%     d_sim_5 = load(strcat('sim_idfriction_25.22/data_joint_',int2str(i-1),'.csv'));
 
 
     subplot(2,6,i)
@@ -14,6 +15,27 @@ for i=1:12
     hold on
     plot(d_sim_1(:,i+2))
     plot(d_sim_2(:,i+2))
-    plot(d_sim_3(:,i+2))
-    plot(d_sim_4(:,i+2))
+%     plot(d_sim_3(:,i+2))
+%     plot(d_sim_4(:,i+2))
+%     plot(d_sim_5(:,i+2))
+end
+
+figure()
+for i=1:12
+    d_real = load(strcat('trial2/data_joint_',int2str(i-1),'.csv'));
+    d_sim_1 = load(strcat('sim_1.32_0.22/data_joint_',int2str(i-1),'.csv'));
+    d_sim_2 = load(strcat('sim_idfriction_0.22/data_joint_',int2str(i-1),'.csv'));
+%     d_sim_3 = load(strcat('sim_idfriction_5.22/data_joint_',int2str(i-1),'.csv'));
+%     d_sim_4 = load(strcat('sim_idfriction_15.22/data_joint_',int2str(i-1),'.csv'));
+%     d_sim_5 = load(strcat('sim_idfriction_25.22/data_joint_',int2str(i-1),'.csv'));
+
+
+    subplot(2,6,i)
+    plot(d_real(:,i+68))
+    hold on
+    plot(d_sim_1(:,i+68))
+    plot(d_sim_2(:,i+68))
+%     plot(d_sim_3(:,i+2))
+%     plot(d_sim_4(:,i+2))
+%     plot(d_sim_5(:,i+2))
 end
