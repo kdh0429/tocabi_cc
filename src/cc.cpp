ba205c3091f93eeb159cecab4157bc6c340552a5
Donghyeon Kim <kdh0429@snu.ac.kr>
2023-10-05 20:29:02 +0900

    On training: Motor 0.8-1.2, with f_z as state

--- src/cc.cpp
+++ src/cc.cpp
@@ -464,11 +464,11 @@ void CustomController::processObservation()
     state_cur_(data_idx) = 0.0;//target_vel_y_;
     data_idx++;
 
-    // state_cur_(data_idx) = rd_cc_.LF_FT(2);
-    // data_idx++;
+    state_cur_(data_idx) = rd_cc_.LF_FT(2);
+    data_idx++;
 
-    // state_cur_(data_idx) = rd_cc_.RF_FT(2);
-    // data_idx++;
+    state_cur_(data_idx) = rd_cc_.RF_FT(2);
+    data_idx++;
 
     for (int i = 0; i <num_actuator_action; i++) 
     {
